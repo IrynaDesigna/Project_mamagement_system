b7c594309682f3e58989351bb6c44e16a347b2db
IrynaDesigna <irynadesigna@gmail.com>
2023-03-09 23:29:49 -0700

    feat: add routing to the header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+
+export class HeaderComponent implements OnInit {
   title = 'Project Management List';
+
+  ngOnInit(): void {}
+
+  constructor(private router: Router, private renderer: Renderer2) {}
+
+  isActive(route: string) {
+    return this.router.url.startsWith(route);
+  }
+
+  @HostListener('window:scroll', ['$event']) onScrollEvent($event: any){
+    const el = document.getElementById('header');
+    if (window.pageYOffset > 56) {
+      console.log(window.pageYOffset > 56);
+
+      if (el) {
+        el.classList.add('scroll');
+      }
+    } else if (window.pageYOffset === 0) {
+      if (el) {
+        el.classList.remove('scroll');
+      }
+    }
+  }
 }
