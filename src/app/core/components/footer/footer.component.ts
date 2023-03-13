79849671a9e5b6ba9cf0233ab5166449b7a670f2
IrynaDesigna <irynadesigna@gmail.com>
2023-03-12 21:27:00 -0600

    feat: add ru-language footer

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LanguageService } from './../../../services/language.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.sass']
 })
 export class FooterComponent {
+  selectedLanguage: string = 'en';
 
+  constructor(private languageService: LanguageService) {
+    this.languageService.language$.subscribe((language) => {
+      this.selectedLanguage = language;
+    });
+  }
 }
