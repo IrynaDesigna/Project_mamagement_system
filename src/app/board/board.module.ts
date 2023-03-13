7bfda2f09cb8ce110d2ddfe724f05b4423d54789
IrynaDesigna <irynadesigna@gmail.com>
2023-03-12 20:50:35 -0600

    feat: board page layout

--- src/app/board/board.module.ts
+++ src/app/board/board.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BoardPageComponent } from './board-page/board-page.component';
 
-
-
 @NgModule({
   declarations: [
     BoardPageComponent
