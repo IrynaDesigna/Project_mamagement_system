5e527928bb6047db6f998ef404699af205dd1b2e
IrynaDesigna <irynadesigna@gmail.com>
2023-03-22 17:40:51 -0600

    fix: language service - edit-profile page

--- src/app/core/pages/edit-profile/edit-profile.component.ts
+++ src/app/core/pages/edit-profile/edit-profile.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguageService } from '../../../services/language.service';
 import { UserService } from 'src/app/services/user.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { InputValidationService } from 'src/app/services/input-validation.service';
 import { CookieService } from 'ngx-cookie-service';
+
 
 
 @Component({
@@ -10,18 +12,30 @@ import { CookieService } from 'ngx-cookie-service';
   templateUrl: './edit-profile.component.html',
   styleUrls: ['./edit-profile.component.sass']
 })
-export class EditProfileComponent {
+export class EditProfileComponent implements OnInit {
+  selectedLanguage: string = 'en';
   localStorage = window.localStorage;
   popupText!: string;
   shouldClosePopup = false;
   shouldShowPopup = false;
+  loginErr!: string;
+  passwordErr!: string;
+  userLogin!: string;
+  userPassword!: string;
 
   constructor(
+    private languageService: LanguageService,
     private userService: UserService,
     private authService: AuthService,
     private cookieService: CookieService,
     public inputValidationService: InputValidationService,
   ){}
+
+  ngOnInit() {
+    this.languageService.language$.subscribe((language) => {
+      this.selectedLanguage = language;
+    });
+  }
 
   onPopupClose() {
     this.shouldClosePopup = true;
@@ -29,7 +43,8 @@ export class EditProfileComponent {
 
   onChangeLogin(userLogin: string) {
     userLogin = userLogin.toLocaleLowerCase().trim();
-    this.inputValidationService.loginValidation(userLogin);
+
+
 
     const userId: string = this.cookieService.get("userId");
 
@@ -38,23 +53,36 @@ export class EditProfileComponent {
         res.name
         const body = {
           name: res.name,
-          login: userLogin,
+          login: "",
           password: this.localStorage.getItem('checkingValue') ?? ''
         }
-        this.userService.resetUser(userId, body).subscribe({
-          next: (Response) => {
-            console.log(Response);
 
-          },
-          error: (error) => {
-            console.log(error);
-          },
-          complete: () => {
-            this.popupText = "Your login is updated!";
-            this.localStorage.setItem("userLogin", userLogin);
-            this.shouldShowPopup = true;
-          }
-        })
+        try {
+          this.inputValidationService.loginValidation(userLogin);
+          body.login = userLogin;
+        }
+        catch(e: any) {
+          if (this.selectedLanguage === 'en') { this.loginErr = `*${e.message}`; }
+          else { this.loginErr = '*Логин может содержать только буквы и цыфры, и должен состоять как минимум из 5ти символов.'};
+        }
+
+        if (body.login !== "") {
+          this.userService.resetUser(userId, body).subscribe({
+            next: (Response) => {
+              console.log(Response);
+
+            },
+            error: (error) => {
+              console.log(error);
+            },
+            complete: () => {
+              this.popupText = "Your login is updated!";
+              this.localStorage.setItem("userLogin", userLogin);
+              this.shouldShowPopup = true;
+              this.userLogin = '';
+            }
+          })
+        }
       },
       error: (err) => {console.log(err);}
     })
@@ -63,25 +91,36 @@ export class EditProfileComponent {
   onUserReset(password: string) {
     const userId: string = this.cookieService.get("userId");
     password = password.trim();
-    this.inputValidationService.passwordValidation(password);
 
     this.userService.getUser(userId).subscribe({
       next: (res) => {
         const body = {
           name: res.name,
           login: res.login,
-          password: password
+          password: ""
         }
 
-        this.userService.resetUser(userId, body).subscribe({
-          next: (Response) => { console.log(Response); },
-          error: (error) => { console.log(error); },
-          complete: () => {
-            this.popupText = "Your password is updated!";
-            this.shouldShowPopup = true;
-            this.localStorage.setItem("checkingValue", password);
-          }
-        })
+        try {
+          this.inputValidationService.passwordValidation(password);
+          body.password = password;
+        }
+        catch(e: any) {
+          if (this.selectedLanguage === 'en') { this.passwordErr = `*${e.message}`; }
+          else { this.passwordErr = '*Пароль должен быть длиной не менее 7 символов и содержать как минимум одну строчную букву, одну заглавную букву, одну цифру и один специальный символ.'};
+        }
+
+        if (body.password !== "") {
+          this.userService.resetUser(userId, body).subscribe({
+            next: (Response) => {},
+            error: (error) => { console.log(error); },
+            complete: () => {
+              this.popupText = "Your password is updated!";
+              this.shouldShowPopup = true;
+              this.localStorage.setItem("checkingValue", password);
+              this.userPassword = '';
+            }
+          })
+        }
       },
       error: (err) => {console.log(err);}
     })
